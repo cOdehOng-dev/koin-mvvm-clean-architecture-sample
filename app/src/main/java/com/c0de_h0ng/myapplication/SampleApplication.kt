af846e908d487a0c3dd062d6adeb72f4c613c8dd
cOdehOng-dev <hsw0323@naver.com>
2022-01-31 01:06:20 +0900

    [Fix] Koin 주입 오류 수정

--- app/src/main/java/com/c0de_h0ng/myapplication/SampleApplication.kt
+++ app/src/main/java/com/c0de_h0ng/myapplication/SampleApplication.kt
@@ -6,9 +6,7 @@ import com.c0de_h0ng.myapplication.di.retrofitModule
 import com.c0de_h0ng.myapplication.di.useCaseModule
 import com.c0de_h0ng.myapplication.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.logger.AndroidLogger
 import org.koin.core.context.startKoin
-import org.koin.core.logger.EmptyLogger
 
 /**
  * Created by c0de_h0ng on 2022/01/30.
@@ -18,12 +16,13 @@ class SampleApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            logger(if (BuildConfig.DEBUG) {
-                AndroidLogger()
-            } else {
-                EmptyLogger()
-            })
+//            logger(if (BuildConfig.DEBUG) {
+//                AndroidLogger()
+//            } else {
+//                EmptyLogger()
+//            })
             androidContext(this@SampleApplication)
+
             modules(
                 retrofitModule,
                 repositoryModule,
